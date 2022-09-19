d021ddfdd1ef91f5344468a9ea1a863edb7f1d8a
martinagabrielli <martina.gabrielli@yahoo.it>
2022-09-19 18:40:11 +0100

    Update cards

--- src/components/Trick.js
+++ src/components/Trick.js
@@ -6,7 +6,11 @@ export default function Trick({details}) {
     return <>
         <h1>{details.name}</h1>
         <p>{details.description}</p>
-        <p>{details.difficulty}/5</p>
+        <div className="d-flex justify-content-center">
+            <p>Difficulty:</p>
+            <span> </span>
+            <p>{details.difficulty}/5</p>
+        </div>
         <button>View more</button>
     </>
 }
