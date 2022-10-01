51e167ec1e7bf0c3a6e959afa3ee0e80c0ef781a
martinagabrielli <martina.gabrielli@yahoo.it>
2022-10-01 17:09:20 +0100

    Update trick component, adjust style, modal testing

--- src/components/Trick.js
+++ src/components/Trick.js
@@ -1,16 +1,27 @@
 import React, {useState} from 'react';
-import tricks from '../tricks';
 
 export default function Trick({details}) {
 
+    function handleTrickButton(e) {
+        console.log(e);
+    }
+
     return <>
-        <h1>{details.name}</h1>
-        <p>{details.description}</p>
-        <div className="d-flex justify-content-center">
-            <p>Difficulty:</p>
-            <span> </span>
-            <p>{details.difficulty}/5</p>
-        </div>
-        <button>View more</button>
+        <section className="trick__single col-lg-4 text-center" key={details.id}>
+            <div class="trick__inner">
+                <h1 className="trick__single-title">{details.name}</h1>
+                <p>{details.description}</p>
+                <div className="d-flex justify-content-center">
+                    <p>Difficulty:</p>
+                    <span> </span>
+                    <p>{details.difficulty}/5</p>
+                </div>
+                <button onClick={handleTrickButton}>View more</button>
+                
+                <div className="trick__modal">
+                    <div>Content</div>
+                </div>
+            </div>
+        </section>
     </>
 }
--- src/components/TricksRollup.js
+++ src/components/TricksRollup.js
@@ -4,11 +4,10 @@ import tricks from '../tricks.js';
 
 export default function TrickRollup() {
     return <>
-        {tricks.map(trick =>
-        <section key={trick.id} className="trick__single col-lg-4 text-center">
-            <div class="trick__inner">
+        <section className="row">
+            {tricks.map(trick =>
                 <Trick details={trick} />
-            </div>
-        </section>)}
+            )}
+        </section>
     </>
 }
