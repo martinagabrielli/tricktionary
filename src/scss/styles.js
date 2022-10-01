ba9012ae3c80c7f74983f0909c7e17646a2f24be
martinagabrielli <martina.gabrielli@yahoo.it>
2022-10-01 16:20:41 +0100

    Add scss files

---
+++ src/scss/styles.js
@@ -0,0 +1,9 @@
+//* BASE
+import './mixins.scss';
+import './variables.scss';
+import './base.scss';
+
+//* COMPONENTS
+import './components/header/header.scss';
+import './components/trick/trick.scss';
+import './components/tricksRollup/tricksRollup.scss';
